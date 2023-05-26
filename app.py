c4fb2e33586dbfc3ddc6f4487c12986ef4a4598e
Ahmed Hafez <35895595+Ahmedtronic@users.noreply.github.com>
2023-05-26 14:11:31 +0300

    final edit

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
 from flask import request
 import os
-from PIL import Image
+0from PIL import Image
 from DeepImageSearch import Load_Data, Search_Setup
+import wget
+import shutil
 
 
 image_list = Load_Data().from_folder(folder_list = ["Data"])
@@ -11,7 +13,11 @@ st = Search_Setup(image_list, model_name="vgg19", pretrained=True)
 file = open("first.txt", "r+")
 x = file.readline()
 if int(x) == 0:
-    st.run_index()
+    response = wget.download("https://github.com/Ahmedtronic/FindSimilar/raw/main/metadata-files/vgg19/image_data_features.pkl")
+    response2 = wget.download("https://github.com/Ahmedtronic/FindSimilar/raw/main/metadata-files/vgg19/image_features_vectors.idx")
+    shutil.move("image_features_vectors.idx", "metadata-files/vgg19/image_features_vectors.idx" )
+    shutil.move("image_data_features.pkl", "metadata-files/vgg19/image_data_features.pkl" )
+    #st.run_index()
     file.write("1")
 
 file.close()
@@ -27,6 +33,8 @@ def is_valid_image(file):
         return False
 
 
+
+
 @app.route('/', methods=['POST', "GET"])
 def index():
     
@@ -34,14 +42,17 @@ def index():
         image = request.files['fileup']
         newimage = Image.open(image)
         newimage.save("Ahmed.jpg")
-        x = st.get_image_metadata_file()
-        similar_images = st.get_similar_images(image_path="Ahmed.jpg", number_of_images=10)
-        #os.remove(newimage.filename) 
-        images  = []
-        for index in similar_images:
-            images.append(similar_images[index])
+        try:
+            x = st.get_image_metadata_file()
+            similar_images = st.get_similar_images(image_path="Ahmed.jpg", number_of_images=10)
+            #os.remove(newimage.filename) 
+            images  = []
+            for index in similar_images:
+                images.append(similar_images[index])
 
-        return jsonify({"Testing": images})
+            return jsonify({"Testing": images})
+        except:
+            return jsonify({"Testing": "Error"})
     else:
         return jsonify({"Error": "No Images"})
             
